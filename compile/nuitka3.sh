6e5db6000cb7884b92ef5c31b0cacee21cd62437
Nickolay Bukreyev <buknik95@yandex.ru>
2017-02-06 03:06:12 +0700

    Add the Nuitka Python compiler

---
+++ compile/nuitka3.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+set -eu
+
+echo main.exe # Don't be fooled, it's still an ELF.
+cat > main.py
+exec /trusted/py3/bin/nuitka \
+    --recurse-all --lto --remove-output -j1 main.py 1>&2
